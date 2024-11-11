cbee401a3932a974ce66f2c4617d08b1669783ff
Mike-3 <Mike.Binnemann@web.de>
2024-11-11 19:25:07 +0100

    add return type

--- lib/tools/crypto_and_encodings/hill/logic/hill.dart
+++ lib/tools/crypto_and_encodings/hill/logic/hill.dart
@@ -4,11 +4,15 @@ import 'dart:core';
 import 'dart:core';
 import 'dart:typed_data';
 
+import 'package:gc_wizard/tools/science_and_technology/divisor/logic/divisor.dart';
+import 'package:gc_wizard/utils/complex_return_types.dart';
 import 'package:gc_wizard/utils/string_utils.dart';
 
-String encryptText(String message, String key) {
+const _fillCharacter = 'X';
+
+StringText encryptText(String message, String key, int matrixSize, String alphabet) {
   message = removeNonLetters(message.toUpperCase());
-  return _hillCipher(message, key);
+  return _encryptHillCipher(message, key, matrixSize, alphabet.length);
   // int n = key.length;
   // int padding = n - plaintext.length() % n;
   // if (padding != n) {
@@ -18,11 +22,11 @@ String encryptText(String message, String key) {
 
 
 // Following function generates the key matrix for the key string
-List<Uint8List> _getKeyMatrix(String key) {
-  var keyMatrix = List<Uint8List>.generate(3, (index) => Uint8List(3));
+List<Uint8List> _getKeyMatrix(String key, int matrixSize) {
+  var keyMatrix = List<Uint8List>.generate(matrixSize, (index) => Uint8List(matrixSize));
   int k = 0;
-  for (int i = 0; i < 3; i++) {
-    for (int j = 0; j < 3; j++) {
+  for (int i = 0; i < matrixSize; i++) {
+    for (int j = 0; j < matrixSize; j++) {
       keyMatrix[i][j] = key.codeUnitAt(k) % 65;
       k++;
     }
@@ -31,7 +35,7 @@ List<Uint8List> _getKeyMatrix(String key) {
 }
 
 // Following function encrypts the message
-List<Uint8List> _matrix_multiplication(List<Uint8List> keyMatrix, List<Uint8List> messageVector) {
+List<Uint8List> _matrixMultiplication(List<Uint8List> keyMatrix, List<Uint8List> messageVector, int alphabetLength) {
   var matrixSize = keyMatrix.length;
   var resultMatrix = List<Uint8List>.generate(matrixSize, (index) => Uint8List(1));
 
@@ -41,13 +45,13 @@ List<Uint8List> _matrix_multiplication(List<Uint8List> keyMatrix, List<Uint8List
       for (int x = 0; x < matrixSize; x++) {
         tmpValue += keyMatrix[i][x] * messageVector[x][j];
       }
-      resultMatrix[i][j] = tmpValue % 26;
+      resultMatrix[i][j] = tmpValue % alphabetLength;
     }
   }
   return resultMatrix;
 }
 
-List<Uint8List>? _invertMatrix(List<Uint8List> matrix) {
+List<Uint8List>? _matrixInvert(List<Uint8List> matrix) {
   int n = matrix.length;
   var augmented = List<List<double>>.generate(n, (index) => List<double>.filled(n * 2, 0));
 
@@ -76,9 +80,7 @@ List<Uint8List>? _invertMatrix(List<Uint8List> matrix) {
       }
     }
 
-    if (augmented[i][i].abs() < 1e-10) {
-      return null;
-    }
+    if (augmented[i][i].abs() < 1e-10) return null;
 
     var pivot = augmented[i][i];
     for (int j = 0; j < 2 * n; j++) {
@@ -105,27 +107,92 @@ List<Uint8List>? _invertMatrix(List<Uint8List> matrix) {
   return result;
 }
 
-
-// Function to implement Hill Cipher
-String _hillCipher(String message, String key) {
-  // Get key matrix from the key string
-  var keyMatrix = _getKeyMatrix(key);
-  var messageVector = List<Uint8List>.generate(3, (index) => Uint8List(1));
+StringText _decryptHillCipher(String message, String key, int matrixSize, int alphabetLength) {
+  // Get inverted key matrix from the key string
+  var keyMatrix = _getKeyMatrix(key, matrixSize);
+  var keyMatrixInverted = _matrixInvert(keyMatrix);
+  if (keyMatrixInverted == null) return StringText('InvalidKey', '');
+  var messageVector = List<Uint8List>.generate(matrixSize, (index) => Uint8List(1));
 
   // Generate vector for the message
-  for (int i = 0; i < 3; i++) {
+  for (int i = 0; i < matrixSize; i++) {
     messageVector[i][0] = message.codeUnitAt(i) % 65;
   }
 
   // Following function generates the encrypted vector
-  var cipherMatrix = _matrix_multiplication(keyMatrix, messageVector);
-
+  var cipherMatrix = _matrixMultiplication(keyMatrix, messageVector, alphabetLength);
   String cipherText = '';
 
   // Generate the encrypted text from the encrypted vector
-  for (int i = 0; i < 3; i++) {
+  for (int i = 0; i < matrixSize; i++) {
     cipherText += String.fromCharCode(cipherMatrix[i][0] + 65);
   }
 
-  return cipherText;
+  return StringText('', cipherText);
+}
+
+
+// Function to implement Hill Cipher
+StringText _encryptHillCipher(String message, String key, int matrixSize, int alphabetLength) {
+  // Get key matrix from the key string
+  var keyMatrix = _getKeyMatrix(key, matrixSize);
+  var messageVector = List<Uint8List>.generate(matrixSize, (index) => Uint8List(1));
+
+  // Generate vector for the message
+  for (int i = 0; i < matrixSize; i++) {
+    messageVector[i][0] = message.codeUnitAt(i) % 65;
+  }
+
+  // Following function generates the encrypted vector
+  var cipherMatrix = _matrixMultiplication(keyMatrix, messageVector, alphabetLength);
+  String cipherText = '';
+
+  // Generate the encrypted text from the encrypted vector
+  for (int i = 0; i < matrixSize; i++) {
+    cipherText += String.fromCharCode(cipherMatrix[i][0] + 65);
+  }
+  var text = _validKeyMatrix(keyMatrix, alphabetLength) ? '' : 'InvalidKey';
+  return StringText(text, cipherText);
+}
+
+bool _validKeyMatrix(List<Uint8List> keyMatrix, int alphabetLength) {
+  var determinante = _matrixDeterminante(keyMatrix).toInt() % alphabetLength;
+  var _divisors = divisors(alphabetLength);
+  for (int i = 0; i < _divisors.length; i++) {
+    if ((determinante % _divisors[i]) == 0) return false;
+  }
+  return true;
+}
+
+double _matrixDeterminante(List<Uint8List> matrix) {
+  int n = matrix.length;
+  var matrixD = List<List<double>>.generate(n, (rowIndex) =>
+    List<double>.generate(matrix[rowIndex].length, (columnIndex) => matrix[rowIndex][columnIndex].toDouble()));
+
+  for (int i = 0; i < n; i++) {
+    if (matrixD[i][i] == 0) {
+      for (int row = i; row < n; row++) {
+        if (matrixD[i][row] != 0) { //flip row
+          for (int k = 0; k < n; k++) {
+            double tmp = matrixD[k][i];
+            matrixD[k][i] = matrixD[k][row];
+            matrixD[k][row] = tmp;
+          }
+        }
+      }
+    }
+    if (matrixD[i][i] == 0) continue;
+    for (int j = i + 1; j < n; j++) {
+      //generate 0s by addition
+      double faktor = -matrixD[j][i] / matrixD[i][i];
+      for (int k = 0; k < n; k++) {
+        matrixD[j][k] += faktor * matrixD[i][k];
+      }
+    }
+  }
+  double result = 1;
+  for (int i = 0; i < n; i++) {
+    result *= matrixD[i][i];
+  }
+  return result;
 }
