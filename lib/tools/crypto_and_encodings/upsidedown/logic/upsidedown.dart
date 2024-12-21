6c29d78f8de14c9f5e543012f063dbe7d31e349e
THOMAS-DESKTOP\thoma <thomas@familiezimmermann.de>
2024-12-21 10:17:25 +0100

    refactor data refactor code - use +=

--- lib/tools/crypto_and_encodings/upsidedown/logic/upsidedown.dart
+++ lib/tools/crypto_and_encodings/upsidedown/logic/upsidedown.dart
@@ -3,110 +3,17 @@
 
 import 'package:gc_wizard/utils/collection_utils.dart';
 
-const Map<String, String> ENCODE_FLIP_ROTATE = {
-  '0': '0',
-  '1': '⇂',
-  //'2': '↊',
-  '2': 'ζ', // u+03B6
-  //'3': '↋',
-  '3': 'Ɛ',
-  '4': '߈',
-  '5': 'ဌ',
-  //'5': '\u20C0',
-  '6': '9',
-  //'7': '𝘓',
-  '7': 'L',
-  '8': '8',
-  '9': '6',
-  'a': 'ɐ',
-  'b': 'q',
-  'c': 'ɔ',
-  'd': 'p',
-  'e': 'ǝ',
-  'f': 'ɟ',
-  'g': 'ᵷ',
-  'h': 'ɥ',
-  'i': '!',
-  'j': 'ſ̣',
-  'k': 'ʞ',
-  'l': 'l',
-  'm': 'ɯ',
-  'n': 'u',
-  'o': 'o',
-  'p': 'd',
-  'q': 'b',
-  'r': 'ɹ',
-  's': 's',
-  't': 'ʇ',
-  'u': 'n',
-  'v': 'ʌ',
-  'w': 'ʍ',
-  'x': 'x',
-  'y': 'ʎ',
-  'z': 'z',
-  'A': 'Ɐ',
-  'B': 'ꓭ',
-  'C': 'Ɔ',
-  'D': 'ꓷ',
-  'E': 'E',
-  'F': 'Ⅎ',
-  'G': '⅁',
-  'H': 'H',
-  'I': 'I',
-  'J': 'ꓩ',
-  'K': 'ꓘ',
-  'L': '⅂',
-  'M': 'ꟽ',
-  'N': 'N',
-  'O': 'O',
-  'P': 'Ԁ',
-  'Q': 'Ꝺ',
-  'R': 'ꓤ',
-  'S': 'S',
-  'T': 'Ʇ',
-  'U': 'Ո',
-  'V': 'Ʌ',
-  'W': 'M',
-  'X': 'X',
-  'Y': '⅄',
-  'Z': 'Z',
-  ' ': ' ',
-  '.': '˙',
-  ':': ':',
-  ',': "'",
-  '-': '-',
-  '_': '‾',
-  '+': '+',
-  '*': '*',
-  '#': '#',
-  '~': '~',
-  '?': '¿',
-  '!': '¡',
-  '/': '\\',
-  '\\': '/',
-  '[': ']',
-  ']': '[',
-  '(': ')',
-  ')': '(',
-  '=': '=',
-  '"': '„',
-  '&': '⅋',
-  '>': '<',
-  '<': '>',
-  '«': '»',
-  '»': '«',
-};
-Map<String, String> DECODE_FLIP_ROTATE = switchMapKeyValue(ENCODE_FLIP_ROTATE);
+part 'package:gc_wizard/tools/crypto_and_encodings/upsidedown/logic/upsidedown_data.dart';
 
 String decodeUpsideDownText(String input) {
   String result = '';
   input = input.replaceAll('ſ̣', 'j').split('').reversed.join('');
 
   for (int i = 0; i < input.length; i++) {
-    if (DECODE_FLIP_ROTATE[input[i]].toString() != 'null') {
-      result = result + DECODE_FLIP_ROTATE[input[i]].toString();
+    if (_DECODE_FLIP_ROTATE[input[i]].toString() != 'null') {
+      result += _DECODE_FLIP_ROTATE[input[i]].toString();
     } else {
-      result = result + input[i];
+      result += input[i];
     }
   }
 
@@ -118,10 +25,10 @@ String encodeUpsideDownText(String input) {
   input = input.split('').reversed.join('');
 
   for (int i = 0; i < input.length; i++) {
-    if (ENCODE_FLIP_ROTATE[input[i]] != null) {
-      result = result + ENCODE_FLIP_ROTATE[input[i]]!;
+    if (_ENCODE_FLIP_ROTATE[input[i]] != null) {
+      result += _ENCODE_FLIP_ROTATE[input[i]]!;
     } else {
-      result = result + input[i];
+      result += input[i];
     }
   }
 
