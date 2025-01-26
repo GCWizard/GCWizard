f87646b56e7c3c23e3dfa28254434a5f9bd31aad
tzimmermann <thomas@familiezimmermann.de>
2025-01-26 09:21:09 +0100

    refactor

--- lib/tools/images_and_files/id3_tag/logic/id3_tag.dart
+++ lib/tools/images_and_files/id3_tag/logic/id3_tag.dart
@@ -1,24 +1,100 @@
+import 'dart:convert';
+import 'dart:typed_data';
+
+import 'package:id3_codec/id3_codec.dart';
+
+class ID3TagList {
+  final List<List<String>> tableTagsHeader;
+  final List<List<String>> tableTagsFrames;
+  final List<List<String>> tableTagsPadding;
+  final List<List<String>> tableTagsMisc;
+  final List<List<String>> tableTagsImages;
+
+  const ID3TagList({
+    required this.tableTagsHeader,
+    required this.tableTagsFrames,
+    required this.tableTagsPadding,
+    required this.tableTagsMisc,
+    required this.tableTagsImages, });
+}
 
 
-List<List<List<String>>>decodeID3String(String dataString){
+ID3TagList decodeID3MetaData(Uint8List bytes){
+  final decoder = ID3Decoder(bytes);
+  final metadata = decoder.decodeSync();
 
+  List<List<String>> _tableTagsHeader = [];
+  List<List<String>> _tableTagsFrames = [];
+  List<List<String>> _tableTagsPadding = [];
+  List<List<String>> _tableTagsImages = [];
+  List<List<String>> _tableTagsMisc = [];
 
-
-  List<String> data = dataString.split('\n');
-  for (int i = 0; i < data.length; i++) {
-    if (data[i].contains('Header')) {
-
-    }
-    else if (data[i].contains('Frames')) {
-
-    }
-    else if (data[i].contains('Padding')) {
-
-    }
-    else {
-
-    }
+  for (var data in metadata) {
+    Map<String, dynamic> dataJSON = data.toTagMap();
+    dataJSON.forEach((key, value) {
+      switch (key) {
+        case "Header": _tableTagsHeader = _buildTableTagsHeader(value as Map<dynamic, dynamic>); break;
+        case "Frames": _tableTagsFrames = _buildTableTagsFrames(value as List<dynamic>); break;
+        case "Padding": _tableTagsPadding = _buildTableTagsPadding(value as Map<dynamic, dynamic>); break;
+        default: _tableTagsMisc = _buildTableTagsMisc(value as Map<dynamic, dynamic>); break;
+      }
+    });
   }
 
-  return [_tableTagsHeader, _tableTagsFrames, _tableTagsPadding, _tableTagsMisc];
+  return ID3TagList(
+      tableTagsHeader: _tableTagsHeader,
+      tableTagsFrames: _tableTagsFrames,
+      tableTagsPadding: _tableTagsPadding,
+      tableTagsMisc: _tableTagsMisc,
+      tableTagsImages: _tableTagsImages);
+}
+
+List<List<String>> _buildTableTagsHeader(Map<dynamic, dynamic> data) {
+  List<List<String>> result = [];
+  data.forEach((key, value) {
+    result.add([key.toString(), value.toString()]);
+  });
+  return result;
+}
+
+List<List<String>> _buildTableTagsPadding(Map<dynamic, dynamic> data) {
+  List<List<String>> result = [];
+  data.forEach((key, value) {
+    result.add([key.toString(), value.toString()]);
+  });
+  return result;
+}
+
+List<List<String>> _buildTableTagsFrames(List<dynamic> data) {
+  List<List<String>> result = [];
+  data.forEach((dataSet) {
+    if (dataSet.runtimeType.toString() == 'LinkedMap<dynamic, dynamic>') {
+      final dataMap = json.decode(json.encode(dataSet)) as Map<String, dynamic>;
+      dataMap.forEach((key, value) {
+        switch (key) {
+          case 'Content':
+            final valueMap = json.decode(json.encode(value)) as Map<String, dynamic>;
+            valueMap.forEach((subKey, value){
+              switch (subKey) {
+                case 'Base64': result.add([key, subKey, value.toString()]);break;
+                default: result.add([key, subKey, value.toString()]);
+              }
+
+            });
+            break;
+          default: result.add([key, value.toString(), '']);
+        }
+      });
+    }
+  });
+
+  return result;
+}
+
+List<List<String>> _buildTableTagsMisc(Map<dynamic, dynamic> data) {
+  List<List<String>> result = [];
+  data.forEach((key, value) {
+    result.add([key.toString(), value.toString()]);
+  });
+  return result;
 }
--- lib/tools/images_and_files/id3_tag/widget/id3_tag.dart
+++ lib/tools/images_and_files/id3_tag/widget/id3_tag.dart
@@ -1,16 +1,13 @@
-import 'dart:convert';
-import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
 import 'package:gc_wizard/application/i18n/logic/app_localizations.dart';
 import 'package:gc_wizard/common_widgets/gcw_expandable.dart';
 import 'package:gc_wizard/common_widgets/outputs/gcw_columned_multiline_output.dart';
-import 'package:id3_codec/id3_codec.dart';
+import 'package:gc_wizard/tools/images_and_files/id3_tag/logic/id3_tag.dart';
 import 'package:gc_wizard/common_widgets/gcw_openfile.dart';
 import 'package:gc_wizard/common_widgets/gcw_snackbar.dart';
 import 'package:gc_wizard/common_widgets/outputs/gcw_default_output.dart';
 import 'package:gc_wizard/utils/file_utils/gcw_file.dart';
-import 'package:intl/intl.dart';
 
 class ID3Tag extends StatefulWidget {
   final GCWFile? file;
@@ -22,13 +19,14 @@ class ID3Tag extends StatefulWidget {
 }
 
 class _ID3TagState extends State<ID3Tag> {
-  List<List<String>> _tableTagsHeader = [];
-  List<List<String>> _tableTagsFrames = [];
-  List<List<String>> _tableTagsPadding = [];
-  List<List<String>> _tableTagsMisc = [];
-
   GCWFile? _file;
-  Uint8List _bytes = Uint8List.fromList([]);
+
+  ID3TagList _ID3TagList = ID3TagList(
+      tableTagsHeader: [],
+      tableTagsFrames: [],
+      tableTagsPadding: [],
+      tableTagsImages: [],
+      tableTagsMisc: []);
 
   var _fileLoaded = false;
 
@@ -38,7 +36,7 @@ class _ID3TagState extends State<ID3Tag> {
 
     if (widget.file != null) {
       _file = widget.file;
-      _bytes = _file!.bytes;
+      _ID3TagList = decodeID3MetaData(_file!.bytes);
       _fileLoaded = true;
     }
   }
@@ -54,31 +52,15 @@ class _ID3TagState extends State<ID3Tag> {
           suppressGallery: false,
           onLoaded: (_file) {
             if (_file == null) {
-              showSnackBar(i18n(context, 'common_loadfile_exception_notloaded'), context);
+              showSnackBar(i18n(context, 'common_loadfile_exception_notloaded'),
+                  context);
               return;
             }
-            _bytes = _file.bytes;
             _fileLoaded = true;
-            _tableTagsHeader = [];
-            _tableTagsFrames = [];
-            _tableTagsPadding = [];
-            _tableTagsMisc = [];
-            final decoder = ID3Decoder(_bytes);
-            final metadata = decoder.decodeSync();
-            for (var data in metadata) {
-              Map<String, dynamic> dataJSON = data.toTagMap();
-              dataJSON.forEach((key, value) {
-                switch (key) {
-                  case "Header": _tableTagsHeader = _buildTableTagsHeader(value as Map<dynamic, dynamic>); break;
-                  case "Frames": _tableTagsFrames = _buildTableTagsFrames(value as List<dynamic>); break;
-                  case "Padding": _tableTagsPadding = _buildTableTagsPadding(value as Map<dynamic, dynamic>); break;
-                  default: _tableTagsMisc = _buildTableTagsMisc(value as Map<dynamic, dynamic>); break;
-                }
-              });
-            }
-            setState(() {
 
-            });
+            _ID3TagList = decodeID3MetaData(_file.bytes);
+
+            setState(() {});
           },
         ),
         _buildOutput()
@@ -96,74 +78,35 @@ class _ID3TagState extends State<ID3Tag> {
         children: [
           GCWExpandableTextDivider(
             text: 'Header',
-            child: GCWColumnedMultilineOutput(data: _tableTagsHeader),
+            child:
+                GCWColumnedMultilineOutput(data: _ID3TagList.tableTagsHeader),
           ),
           GCWExpandableTextDivider(
             text: 'Frames',
-            child: GCWColumnedMultilineOutput(data: _tableTagsFrames),
+            child:
+                GCWColumnedMultilineOutput(data: _ID3TagList.tableTagsFrames),
           ),
           GCWExpandableTextDivider(
             text: 'Padding',
-            child: GCWColumnedMultilineOutput(data: _tableTagsPadding),
+            child:
+                GCWColumnedMultilineOutput(data: _ID3TagList.tableTagsPadding),
           ),
-          _tableTagsMisc.isNotEmpty ? GCWExpandableTextDivider(
-            text: '?',
-            child: GCWColumnedMultilineOutput(data: _tableTagsMisc),
-          ) : Container(),
+          _ID3TagList.tableTagsImages.isNotEmpty
+              ? _buildImageOutput(_ID3TagList.tableTagsImages)
+              : Container(),
+          _ID3TagList.tableTagsMisc.isNotEmpty
+              ? GCWExpandableTextDivider(
+                  text: '?',
+                  child: GCWColumnedMultilineOutput(
+                      data: _ID3TagList.tableTagsMisc),
+                )
+              : Container(),
         ],
       ),
     );
   }
 
-List<List<String>> _buildTableTagsHeader(Map<dynamic, dynamic> data) {
-  List<List<String>> result = [];
-  data.forEach((key, value) {
-    result.add([key.toString(), value.toString()]);
-  });
-  return result;
-}
-
-  List<List<String>> _buildTableTagsPadding(Map<dynamic, dynamic> data) {
-    List<List<String>> result = [];
-    data.forEach((key, value) {
-      result.add([key.toString(), value.toString()]);
-    });
-    return result;
-  }
-
-  List<List<String>> _buildTableTagsFrames(List<dynamic> data) {
-    List<List<String>> result = [];
-    data.forEach((dataSet) {
-      if (dataSet.runtimeType.toString() == 'LinkedMap<dynamic, dynamic>') {
-        final dataMap = json.decode(json.encode(dataSet)) as Map<String, dynamic>;
-        dataMap.forEach((key, value) {
-          switch (key) {
-            case 'Content':
-              final valueMap = json.decode(json.encode(value)) as Map<String, dynamic>;
-              valueMap.forEach((subKey, value){
-                switch (subKey) {
-                  case 'Base64': result.add([key, subKey, value.toString()]);break;
-                  default: result.add([key, subKey, value.toString()]);
-                }
-
-              });
-              break;
-            default: result.add([key, value.toString(), '']);
-          }
-        });
-      }
-    });
-
-    return result;
-  }
-
-  List<List<String>> _buildTableTagsMisc(Map<dynamic, dynamic> data) {
-    List<List<String>> result = [];
-    data.forEach((key, value) {
-      result.add([key.toString(), value.toString()]);
-    });
-    return result;
+  Widget _buildImageOutput(List<List<String>> imageData) {
+    return Container();
   }
 }
-
-
