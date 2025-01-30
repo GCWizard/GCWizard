d2a2c6fb10818be6d1f254044790a1c80ef5d344
olliklee <okleemann@gmail.com>
2025-01-30 18:48:09 +0100

    bit shift method to inexact

--- lib/tools/science_and_technology/divisor/logic/divisor.dart
+++ lib/tools/science_and_technology/divisor/logic/divisor.dart
@@ -1,27 +1,32 @@
-int _intSqrt(int number) {
-  int result = 0;
-  int bit = 1 << 30;
+import 'dart:math';
 
-  while (bit > number) {
-    bit >>= 2;
-  }
 
-  while (bit != 0) {
-    if (number >= result + bit) {
-      number -= result + bit;
-      result = (result >> 1) + bit;
-    } else {
-      result >>= 1;
-    }
-    bit >>= 2;
-  }
+// int _intSqrt(int number) {
+//   int result = 0;
+//   int bit = 1 << 30;
+//
+//   while (bit > number) {
+//     bit >>= 2;
+//   }
+//
+//   while (bit != 0) {
+//     if (number >= result + bit) {
+//       number -= result + bit;
+//       result = (result >> 1) + bit;
+//     } else {
+//       result >>= 1;
+//     }
+//     bit >>= 2;
+//   }
+//
+//   return result;i
+// }
 
-  return result;
-}
 
 List<int> divisors(int number) {
   List<int> divisorList = <int>[];
-  int sqrtNumber = _intSqrt(number);
+  // int sqrtNumber = _intSqrt(number);
+  int sqrtNumber = sqrt(number).toInt();
 
   for (int i = 1; i <= sqrtNumber; i++) {
     if (number % i == 0) {
