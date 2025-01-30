ef973eafc8d4ed085223367ae1a3bfb39e0b507d
Mike-3 <Mike.Binnemann@web.de>
2025-01-30 05:56:58 +0100

    combine solver

--- lib/tools/games/verbal_arithmetic/logic/alphametics.dart
+++ lib/tools/games/verbal_arithmetic/logic/alphametics.dart
@@ -7,6 +7,7 @@ import 'package:gc_wizard/utils/complex_return_types.dart';
 import 'package:math_expressions/math_expressions.dart';
 
 part 'package:gc_wizard/tools/games/verbal_arithmetic/logic/alphametics_addition.dart';
+part 'package:gc_wizard/tools/games/verbal_arithmetic/logic/symbolism.dart';
 
 Future<VerbalArithmeticOutput?> solveAlphameticsAsync(GCWAsyncExecuterParameters jobData) async {
   if (jobData.parameters is! VerbalArithmeticJobData) {
@@ -14,7 +15,7 @@ Future<VerbalArithmeticOutput?> solveAlphameticsAsync(GCWAsyncExecuterParameters
   }
   var data = jobData.parameters as VerbalArithmeticJobData;
 
-  var output = solveAlphametic(data.equations.first, data.allSolutions, data.allowLeadingZeros,
+  var output = solveAlphametic(data.equations, data.allSolutions, data.allowLeadingZeros,
       sendAsyncPort: jobData.sendAsyncPort);
 
   if (jobData.sendAsyncPort != null) jobData.sendAsyncPort!.send(output);
@@ -30,30 +31,37 @@ int _stepSize = 1;
 int _nextSendStep = 1;
 SendPort? _sendAsyncPort;
 
-VerbalArithmeticOutput? solveAlphametic(String equation, bool allSolutions, bool allowLeadingZeros,
+VerbalArithmeticOutput? solveAlphametic(List<String> equations, bool allSolutions, bool allowLeadingZeros,
     {SendPort? sendAsyncPort}) {
-  var _equation = Equation(equation, singleLetter: true);
-  if (!_equation.validFormula) {
-    return VerbalArithmeticOutput(equations: [_equation], solutions: [], error: 'InvalidEquation');
+  var _equations = equations.map((equation) => Equation(equation, singleLetter: true, rearrange: equations.length > 1)).toList();
+  var notValid = _equations.any((equation) => !equation.validFormula);
+  if (notValid || _equations.isEmpty) {
+    return VerbalArithmeticOutput(equations: _equations, solutions: [], error: 'InvalidEquation');
   }
 
-  if (_equation.usedMembers.length > 10) {
+  final Set<String> variables = {};
+  for (var equation in _equations) {
+    variables.addAll(equation.usedMembers);
+  }
+  if (variables.length > 10) {
     return VerbalArithmeticOutput(equations: [], solutions: [], error: 'TooManyLetters');
   }
 
   _allSolutions = allSolutions;
   _allowLeadingZeros = allowLeadingZeros;
 
-  _totalPermutations = _calculatePossibilities(_equation.usedMembers.length);
+  _totalPermutations = _calculatePossibilities(variables.length);
   _currentCombination = 0;
   _stepSize  = max(_totalPermutations ~/ 100, 1);
   _nextSendStep = _stepSize;
   _sendAsyncPort = sendAsyncPort;
 
-  if (_equation.onlyAddition) {
-    return _solveAlphameticAdd(_equation);
+  if (_equations.length > 1) {
+    return _solveSymbolism(_equations, variables);
+  } else if (_equations.first.onlyAddition) {
+    return _solveAlphameticAdd(_equations.first);
   } else {
-    return _solveAlphametic(_equation);
+    return _solveAlphametic(_equations.first);
   }
 }
 
@@ -166,28 +174,54 @@ void main() {
   _allSolutions = true;
   var startTime = DateTime.now();
   // Beispiel: SEND + MORE = MONEY
-  solveAlphametic("SEND+MORE=MONEY", false, false);
+  solveAlphametic(["SEND+MORE=MONEY"], false, false);
   print(DateTime.now().difference(startTime).inMilliseconds.toString() + 'ms');
 
   startTime = DateTime.now();
   // Beispiel: ELEVEN + NINE + FIVE + FIVE = THIRTY
-  solveAlphametic("ELEVEN+NINE+FIVE+FIVE=THIRTY", false, false);
+  solveAlphametic(["ELEVEN+NINE+FIVE+FIVE=THIRTY"], false, false);
   print(DateTime.now().difference(startTime).inMilliseconds.toString() + 'ms');
 
   startTime = DateTime.now();
   // Beispiel: ENIGMA / M = TIMES
-  solveAlphametic("ENIGMA/M=TIMES", false, false);
+  solveAlphametic(["ENIGMA/M=TIMES"], false, false);
   print(DateTime.now().difference(startTime).inMilliseconds.toString() + 'ms');
 
   startTime = DateTime.now();
   // Beispiel: "BASE + BALL = GAMES
-  solveAlphametic("BASE + BALL = GAMES", false, false);
+  solveAlphametic(["BASE + BALL = GAMES"], false, false);
   print(DateTime.now().difference(startTime).inMilliseconds.toString() + 'ms');
 
   startTime = DateTime.now();
   // Beispiel: THIS + A + FIRE... (langes Beispiel)
   solveAlphametic(
-      "THIS+A+FIRE+THEREFORE+FOR+ALL+HISTORIES+I+TELL+A+TALE+THAT+FALSIFIES+ITS+TITLE+TIS+A+LIE+THE+TALE+OF+THE+LAST+FIRE+HORSES+LATE+AFTER+THE+FIRST+FATHERS+FORESEE+THE+HORRORS+THE+LAST+FREE+TROLL+TERRIFIES+THE+HORSES+OF+FIRE+THE+TROLL+RESTS+AT+THE+HOLE+OF+LOSSES+IT+IS+THERE+THAT+SHE+STORES+ROLES+OF+LEATHERS+AFTER+SHE+SATISFIES+HER+HATE+OFF+THOSE+FEARS+A+TASTE+RISES+AS+SHE+HEARS+THE+LEAST+FAR+HORSE+THOSE+FAST+HORSES+THAT+FIRST+HEAR+THE+TROLL+FLEE+OFF+TO+THE+FOREST+THE+HORSES+THAT+ALERTS+RAISE+THE+STARES+OF+THE+OTHERS+AS+THE+TROLL+ASSAILS+AT+THE+TOTAL+SHIFT+HER+TEETH+TEAR+HOOF+OFF+TORSO+AS+THE+LAST+HORSE+FORFEITS+ITS+LIFE+THE+FIRST+FATHERS+HEAR+OF+THE+HORRORS+THEIR+FEARS+THAT+THE+FIRES+FOR+THEIR+FEASTS+ARREST+AS+THE+FIRST+FATHERS+RESETTLE+THE+LAST+OF+THE+FIRE+HORSES+THE+LAST+TROLL+HARASSES+THE+FOREST+HEART+FREE+AT+LAST+OF+THE+LAST+TROLL+ALL+OFFER+THEIR+FIRE+HEAT+TO+THE+ASSISTERS+FAR+OFF+THE+TROLL+FASTS+ITS+LIFE+SHORTER+AS+STARS+RISE+THE+HORSES+REST+SAFE+AFTER+ALL+SHARE+HOT+FISH+AS+THEIR+AFFILIATES+TAILOR+A+ROOFS+FOR+THEIR+SAFE=FORTRESSES", false, false);
+      ["THIS+A+FIRE+THEREFORE+FOR+ALL+HISTORIES+I+TELL+A+TALE+THAT+FALSIFIES+ITS+TITLE+TIS+A+LIE+THE+TALE+OF+THE+LAST+FIRE+HORSES+LATE+AFTER+THE+FIRST+FATHERS+FORESEE+THE+HORRORS+THE+LAST+FREE+TROLL+TERRIFIES+THE+HORSES+OF+FIRE+THE+TROLL+RESTS+AT+THE+HOLE+OF+LOSSES+IT+IS+THERE+THAT+SHE+STORES+ROLES+OF+LEATHERS+AFTER+SHE+SATISFIES+HER+HATE+OFF+THOSE+FEARS+A+TASTE+RISES+AS+SHE+HEARS+THE+LEAST+FAR+HORSE+THOSE+FAST+HORSES+THAT+FIRST+HEAR+THE+TROLL+FLEE+OFF+TO+THE+FOREST+THE+HORSES+THAT+ALERTS+RAISE+THE+STARES+OF+THE+OTHERS+AS+THE+TROLL+ASSAILS+AT+THE+TOTAL+SHIFT+HER+TEETH+TEAR+HOOF+OFF+TORSO+AS+THE+LAST+HORSE+FORFEITS+ITS+LIFE+THE+FIRST+FATHERS+HEAR+OF+THE+HORRORS+THEIR+FEARS+THAT+THE+FIRES+FOR+THEIR+FEASTS+ARREST+AS+THE+FIRST+FATHERS+RESETTLE+THE+LAST+OF+THE+FIRE+HORSES+THE+LAST+TROLL+HARASSES+THE+FOREST+HEART+FREE+AT+LAST+OF+THE+LAST+TROLL+ALL+OFFER+THEIR+FIRE+HEAT+TO+THE+ASSISTERS+FAR+OFF+THE+TROLL+FASTS+ITS+LIFE+SHORTER+AS+STARS+RISE+THE+HORSES+REST+SAFE+AFTER+ALL+SHARE+HOT+FISH+AS+THEIR+AFFILIATES+TAILOR+A+ROOFS+FOR+THEIR+SAFE=FORTRESSES"], false, false);
+  print(DateTime.now().difference(startTime).inMilliseconds.toString() + 'ms');
+
+  List<String> equations = [
+    "ABCB+DEAF=GFFB",
+    "AEEF+AHG=AGIG",
+    "EBB*AH=HGCF",
+    "ABCB-AEEF=EBB",
+    "DEAF/AHG=AH",
+    "GFFB+AGIG=HGCF"
+  ];
+
+  startTime = DateTime.now();
+  solveAlphametic(equations, false, false);
+  print(DateTime.now().difference(startTime).inMilliseconds.toString() + 'ms');
+
+  equations = [
+  "GJ*DJ=LBAC",
+  "BJKD+BCCK=DJKB",
+  "BJLH-GF=BHJL",
+  "BJKD-GJ=BJLH",
+  "BCCK/DJ=GF",
+  "DJKB-LBAC=BHJL"
+  ];
+
+  startTime = DateTime.now();
+  solveAlphametic(equations, false, false);
   print(DateTime.now().difference(startTime).inMilliseconds.toString() + 'ms');
 
   // Gesamtanzahl der Permutationen: 1814400
--- lib/tools/games/verbal_arithmetic/logic/symbolism.dart
+++ lib/tools/games/verbal_arithmetic/logic/symbolism.dart
@@ -1,64 +1,12 @@
-import 'dart:collection';
-import 'dart:isolate';
-import 'dart:math';
-import 'package:gc_wizard/common_widgets/async_executer/gcw_async_executer_parameters.dart';
-import 'package:gc_wizard/tools/games/verbal_arithmetic/logic/helper.dart';
-import 'package:gc_wizard/utils/complex_return_types.dart';
-import 'package:math_expressions/math_expressions.dart';
+part of 'package:gc_wizard/tools/games/verbal_arithmetic/logic/alphametics.dart';
 
-Future<VerbalArithmeticOutput?> solveCryptogramAsync(GCWAsyncExecuterParameters jobData) async {
-  if (jobData.parameters is! VerbalArithmeticJobData) {
-    return null;
-  }
-  var data = jobData.parameters as VerbalArithmeticJobData;
 
-  var output = solveSymbolism(data.equations, data.allSolutions, data.allowLeadingZeros,
-      sendAsyncPort: jobData.sendAsyncPort);
-
-  if (jobData.sendAsyncPort != null) jobData.sendAsyncPort!.send(output);
-
-  return output;
-}
-
-bool _allSolutions = false;
-bool _allowLeadingZeros = false;
-int _totalPermutations = 0;
-int _currentCombination = 0;
-int _stepSize = 1;
-int _nextSendStep = 1;
-SendPort? _sendAsyncPort;
-List<HashMap<String, int>> _solutions = [];
-
-VerbalArithmeticOutput? solveSymbolism(List<String> equations, bool allSolutions, bool allowLeadingZeros,
-    {SendPort? sendAsyncPort}) {
-  var _equations = equations.map((equation) => Equation(equation, singleLetter: true, rearrange: true)).toList();
-  var notValid = _equations.any((equation) => !equation.validFormula);
-  if (notValid || _equations.isEmpty) {
-    return VerbalArithmeticOutput(equations: _equations, solutions: [], error: 'InvalidEquation');
-  }
-
-  _allSolutions = allSolutions;
-  _allowLeadingZeros = allowLeadingZeros;
-  return _solveSymbolism(_equations, sendAsyncPort);
-}
-
-VerbalArithmeticOutput? _solveSymbolism(List<Equation> equations, SendPort? sendAsyncPort) {
-  final Set<String> variables = {};
-  for (var equation in equations) {
-    variables.addAll(equation.usedMembers);
-  }
-
-  final variableList = variables.toList()..sort();
-
+VerbalArithmeticOutput? _solveSymbolism(List<Equation> equations, Set<String> variables) {
+  final variableList = variables.toList();
   final digits = List.generate(10, (i) => i);
 
   _solutions.clear();
   equations = _sortEquations(equations);
-
-  _totalPermutations = _calculatePossibilities(digits.length);
-  _currentCombination = 0;
-  _stepSize  = max(_totalPermutations ~/ 100, 1);
-  _nextSendStep = _stepSize;
 
   __solveSymbolism(equations, HashMap<String, int>(), variableList, digits);
 
@@ -69,7 +17,7 @@ bool __solveSymbolism(List<Equation> equations, HashMap<String, int> mapping, Li
     List<int> availableValues) {
 
   if (remainingVariables.isEmpty) {
-    if (_evaluateEquation(mapping, equations)) {
+    if (_evaluateEquationSymbolism(mapping, equations)) {
       if (!_allowLeadingZeros && _hasLeadingZeros(mapping, equations)) {
         return false;
       }
@@ -92,7 +40,7 @@ bool __solveSymbolism(List<Equation> equations, HashMap<String, int> mapping, Li
     _currentCombination++;
     _sendProgress();
 
-    if (!_evaluateEquation(mapping, equations)) {
+    if (!_evaluateEquationSymbolism(mapping, equations)) {
       mapping.remove(variable);
       // _currentCombination += _calculatePossibilities(availableValues.length, remainingVariables.length);
       _sendProgress();
@@ -111,19 +59,6 @@ bool __solveSymbolism(List<Equation> equations, HashMap<String, int> mapping, Li
   return false;
 }
 
-void _sendProgress() {
-  if (_sendAsyncPort != null && _currentCombination >= _nextSendStep) {
-    _nextSendStep += _stepSize;
-    _sendAsyncPort?.send(DoubleText(PROGRESS, _currentCombination / _totalPermutations));
-  }
-}
-
-/// Calculating the number of possible permutations
-int _calculatePossibilities(int lettersCount) {
-  return factorial(10) ~/ factorial(10 - lettersCount);
-}
-
-
 List<Equation> _sortEquations(List<Equation> equations) {
 
   List<MapEntry<Equation, int>> equationScores = equations.map((equation) {
@@ -139,9 +74,7 @@ List<Equation> _sortEquations(List<Equation> equations) {
   return equationScores.map((entry) => entry.key).toList();
 }
 
-final _cm = ContextModel();
-
-bool _evaluateEquation(Map<String, int> mapping, List<Equation> equations) {
+bool _evaluateEquationSymbolism(Map<String, int> mapping, List<Equation> equations) {
   for (var equation in equations) {
 
     if (!mapping.keys.toSet().containsAll(equation.usedMembers)) {
@@ -170,37 +103,37 @@ bool _hasLeadingZeros(Map<String, int> mapping, List<Equation> equations) {
   return false;
 }
 
-void main() {
-  // Beispielgleichungen
-  List<String> equations = [
-    "ABCB+DEAF=GFFB",
-    "AEEF+AHG=AGIG",
-    "EBB*AH=HGCF",
-    "ABCB-AEEF=EBB",
-    "DEAF/AHG=AH",
-    "GFFB+AGIG=HGCF"
-  ];
-
-  var startTime = DateTime.now();
-  // Lösen
-  solveSymbolism(equations, false, false);
-  print(DateTime.now().difference(startTime).inMilliseconds.toString() + 'ms');
-
-  equations = [
-  "GJ*DJ=LBAC",
-  "BJKD+BCCK=DJKB",
-  "BJLH-GF=BHJL",
-  "BJKD-GJ=BJLH",
-  "BCCK/DJ=GF",
-  "DJKB-LBAC=BHJL"
-  ];
-
-  startTime = DateTime.now();
-  // Lösen
-  solveSymbolism(equations, false, false);
-  print(DateTime.now().difference(startTime).inMilliseconds.toString() + 'ms');
-
-}
+// void main() {
+//   // Beispielgleichungen
+//   List<String> equations = [
+//     "ABCB+DEAF=GFFB",
+//     "AEEF+AHG=AGIG",
+//     "EBB*AH=HGCF",
+//     "ABCB-AEEF=EBB",
+//     "DEAF/AHG=AH",
+//     "GFFB+AGIG=HGCF"
+//   ];
+//
+//   var startTime = DateTime.now();
+//   // Lösen
+//   solveSymbolism(equations, false, false);
+//   print(DateTime.now().difference(startTime).inMilliseconds.toString() + 'ms');
+//
+//   equations = [
+//   "GJ*DJ=LBAC",
+//   "BJKD+BCCK=DJKB",
+//   "BJLH-GF=BHJL",
+//   "BJKD-GJ=BJLH",
+//   "BCCK/DJ=GF",
+//   "DJKB-LBAC=BHJL"
+//   ];
+//
+//   startTime = DateTime.now();
+//   // Lösen
+//   solveSymbolism(equations, false, false);
+//   print(DateTime.now().difference(startTime).inMilliseconds.toString() + 'ms');
+//
+// }
 
 // Solution for Example 1: {A: 1, B: 6, C: 9, D: 2, E: 3, F: 0, G: 4, H: 5, I: 8}
 // 2532ms
@@ -211,63 +144,4 @@ void main() {
 // 233ms
 // Lösung gefunden: BJKD+BCCK=DJKB {F: 4, A: 0, J: 7, K: 8, B: 1, H: 6, D: 3, C: 9, G: 5, L: 2} 98282% 1000000000
 // 233ms
-
-// import 'package:math_expressions/math_expressions.dart';
-//
-// void main() {
-//   // Beispiel: Lösen des Systems:
-//   // A * B = 1428
-//   // C - D = 12
-//   // A * C = 840
-//   // B - D = 33
-//
-//   // Wir versuchen, das System numerisch zu lösen:
-//   double A = 0, B = 0, C = 0, D = 0;
-//   double tolerance = 0.0001; // Toleranz für die Lösung
-//   double learningRate = 0.01; // Lernrate für die numerische Lösung
-//
-//   // Startwerte für die Variablen
-//   A = 10;
-//   B = 10;
-//   C = 10;
-//   D = 10;
-//
-//   // Hilfsfunktion zur Berechnung des Fehlers
-//   double error() {
-//     double eq1 = A * B - 1428;
-//     double eq2 = C - D - 12;
-//     double eq3 = A * C - 840;
-//     double eq4 = B - D - 33;
-//
-//     // Fehler ist die Summe der Quadrate der Abweichungen
-//     return eq1 * eq1 + eq2 * eq2 + eq3 * eq3 + eq4 * eq4;
-//   }
-//
-//   // Iterativer Ansatz (Gradientenabstieg)
-//   while (error() > tolerance) {
-//     // Berechne die Ableitungen (Gradienten) für jede Gleichung
-//     double eq1 = A * B - 1428;
-//     double eq2 = C - D - 12;
-//     double eq3 = A * C - 840;
-//     double eq4 = B - D - 33;
-//
-//     // Berechne die partiellen Ableitungen (Zahlenbeispiel)
-//     double dA = B * eq1 + C * eq3;
-//     double dB = A * eq1 + eq4;
-//     double dC = A * eq3 - eq2;
-//     double dD = eq2 + eq4;
-//
-//     // Update der Werte durch Subtraktion der Lernrate * Ableitung
-//     A -= learningRate * dA;
-//     B -= learningRate * dB;
-//     C -= learningRate * dC;
-//     D -= learningRate * dD;
-//
-//     // Debug-Ausgabe
-//     print("A: $A, B: $B, C: $C, D: $D, Fehler: ${error()}");
-//   }
-//
-//   // Endergebnis
-//   print("Lösung: A = $A, B = $B, C = $C, D = $D");
-// }
 
