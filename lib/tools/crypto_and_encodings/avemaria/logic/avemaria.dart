aaf243791d2497f0e60e0f45a20d266399cadf61
Olli <102247940+olliklee@users.noreply.github.com>
2025-03-04 20:48:36 +0100

    added test

--- lib/tools/crypto_and_encodings/avemaria/logic/avemaria.dart
+++ lib/tools/crypto_and_encodings/avemaria/logic/avemaria.dart
@@ -2,21 +2,29 @@ import 'dart:math';
 
 part 'package:gc_wizard/tools/crypto_and_encodings/avemaria/logic/avemaria_data.dart';
 
+RegExp _delimiters = RegExp('\t|\n|\r|\r\n');
+
 String decodeAveMaria(String chiffre) {
   List<String> result = [];
-  List<String> code = chiffre.toLowerCase().replaceAll(RegExp('\n|\r|\r\n'), "  ").split(RegExp(' {2:}'));
 
-  for (String word in code) {
-    for (String letter in word.split(' ')) {
-       result.add(_AVE_MARIA[letter] ?? ' ');
+  List<String> code = chiffre.toLowerCase()
+      .replaceAll(_delimiters, ' ')
+      .split(RegExp(' {2,}'));
+
+  for (String line in code) {
+    for (var word in line.split(' ')){
+      result.add(_AVE_MARIA[word] ?? ' ');
     }
+    result.add(' '); // if double space or delimiter, place a space
   }
-  return result.join('');
+
+  return result.join('').trim();
 }
 
 String encodeAveMaria(String plain) {
   List<String> result = [];
-  List<String> code = plain.toUpperCase().split(' ');
+  List<String> code = plain.toUpperCase().replaceAll(_delimiters, '  ')
+      .split(RegExp(r' {2,}'));
   var aveMaria = _AVE_MARIA.entries.toList();
   aveMaria.addAll(_AVE_MARIA_ENCODE_EXTENSION);
 
