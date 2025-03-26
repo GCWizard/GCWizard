12ea4d7ae4765b4e32ededf4c4a6538776bc6b54
Mike-3 <Mike.Binnemann@web.de>
2025-03-26 07:27:45 +0100

    Update larrabee.dart

--- lib/tools/crypto_and_encodings/larrabee/logic/larrabee.dart
+++ lib/tools/crypto_and_encodings/larrabee/logic/larrabee.dart
@@ -1,19 +1,57 @@
+import 'package:collection/collection.dart';
 import 'package:gc_wizard/tools/crypto_and_encodings/vigenere/logic/vigenere.dart';
-import 'package:gc_wizard/utils/alphabets.dart';
+import 'package:gc_wizard/utils/collection_utils.dart';
 
 String encryptLarrabee(String input, String key) {
+  input = replaceNumbers(input);
   return encryptVigenere(input, key, false);
 }
 
 String decryptLarrabee(String input, String key) {
-  return decryptVigenere(input, key, false);
+  var decodedText = restoreNumbers(input);
+  return restoreNumbers(decodedText);
 }
 
-String replaceNumbers(String input) {
-  var replacment = alphabet_AZ;
-  var matches = RegExp(r'\d+').allMatches(input).toList();
-  for (var match in matches.reversed) {
-var number =match.toString().split('').map((number) => replacment[])
-  }
+const String _numberIdentifier = 'Q';
+const Map<String, String> _numbers = {
+  'A': '1',
+  'B': '2',
+  'C': '3',
+  'D': '4',
+  'E': '5',
+  'F': '6',
+  'G': '7',
+  'H': '8',
+  'I': '9',
+  'J': '0',
+};
 
+String replaceNumbers(String input) {
+  var matches = RegExp(r'\d+').allMatches(input).toList();
+  var numbers = switchMapKeyValue(_numbers);
+
+  for (var match in matches.reversed) {
+    var numbersCoded = match.toString().split('').mapIndexed((index, number) =>
+        index < 10 ? numbers[number] : number).join();
+    numbersCoded = _numberIdentifier +
+        (numbersCoded.length < 10 ? numbers[numbersCoded.length.toString()] ?? '' : numbers[9.toString()] ?? '') +
+        numbersCoded;
+    input = input.replaceRange(match.start, match.end, numbersCoded);
+  }
+  return input;
+}
+
+String restoreNumbers(String input) {
+  var matches = RegExp(r'['+ _numberIdentifier +']([A-I])([A-J]+)').allMatches(input).toList();
+
+  for (var match in matches.reversed) {
+    var count = (_numbers[match.group(1)] ?? '0') as int;
+
+    if (count > 0 && match.group(2)!.length >= count) {
+      var numbersDecoded = match.group(2).toString().split('').mapIndexed((index, number) =>
+          index < count ? _numbers[number] : number).join();
+      input = input.replaceRange(match.start, match.end, numbersDecoded);
+    }
+  }
+  return input;
 }
