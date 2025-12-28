e6afed157c7950ad6abafbd57db78957977a4ad3
tmz <thomas@familiezimmermann.de>
2025-12-28 09:41:01 +0100

    fix exception when increasing end/decreasing start of item list
    work in progress

--- lib/common_widgets/gcw_custom_date_picker.dart
+++ lib/common_widgets/gcw_custom_date_picker.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:gc_wizard/common_widgets/dropdowns/gcw_dropdown.dart';
 import 'package:gc_wizard/common_widgets/spinners/gcw_dropdown_spinner.dart';
 import 'package:gc_wizard/common_widgets/spinners/gcw_integer_spinner.dart';
 import 'package:gc_wizard/common_widgets/spinners/spinner_constants.dart';
@@ -136,7 +135,7 @@ class _GCWCustomDatePickerState extends State<GCWCustomDatePicker> {
         index: _currentMonth,
         layout: SpinnerLayout.VERTICAL,
         items: MONTH_NAMES[type]!.entries.map((entry) {
-          return GCWDropDownMenuItem(value: entry.key, child: entry.value);
+          return entry.value;
         }).toList(),
         onChanged: (value) {
           setState(() {
