09e7dceb5aa9a578db4b3a04775c4edc73674fee
tmz <thomas@familiezimmermann.de>
2026-02-11 23:51:55 +0100

    add tests

--- test/tools/science_and_technology/euclidic_triangle/logic/triangle_sides_tests.dart
+++ test/tools/science_and_technology/euclidic_triangle/logic/triangle_sides_tests.dart
@@ -4,23 +4,23 @@ import 'package:gc_wizard/tools/science_and_technology/euclidic_triangle/logic/t
 import 'triangle_tests.dart';
 
 void main() {
-  group("triangle.triangleAnglesXY:", () {
+  group("triangle.triangleSidesXY:", () {
     List<Map<String, Object?>> _inputsToExpected = [
       {'inputA': XYPoint(x: 0, y: 0), 'inputB': XYPoint(x: 0, y: 0), 'inputC': XYPoint(x: 0, y: 0),
-        'expectedOutput': Angles(alpha: double.nan, beta: double.nan, gamma: double.nan)},
+        'expectedOutput': Sides(a: 0.0, b: 0.0, c: 0.0)},
       {'inputA': XYPoint(x: 1, y: 1), 'inputB': XYPoint(x: 1, y: 1), 'inputC': XYPoint(x: 1, y: 1),
-        'expectedOutput': Angles(alpha: double.nan, beta: double.nan, gamma: double.nan)},
+        'expectedOutput': Sides(a: 0.0, b: 0.0, c: 0.0)},
       {'inputA': XYPoint(x: 1, y: 1), 'inputB': XYPoint(x: 2, y: 2), 'inputC': XYPoint(x: 3, y: 3),
-        'expectedOutput': Angles(alpha: 8.537736462515939e-7, beta: 179.99999914622634, gamma: 8.537736462515939e-7)},
+        'expectedOutput': Sides(a: 1.4142135623730951, b: 2.8284271247461903, c: 1.4142135623730951)},
 
       {'inputA': XYPoint(x: 0, y: 0), 'inputB': XYPoint(x: 0, y: 3), 'inputC': XYPoint(x: 4, y: 0),
-        'expectedOutput': Angles(alpha: 90.0, beta: 53.13010235415599, gamma: 36.86989764584401)},
+        'expectedOutput': Sides(a: 5, b: 4, c: 3)},
     ];
 
     for (var elem in _inputsToExpected) {
       test('input: ${toString(elem['inputA'])} ${toString(elem['inputB'])} ${toString(elem['inputC'])}', () {
-        var _actual = triangleAnglesXY(elem['inputA'] as XYPoint, elem['inputB'] as XYPoint, elem['inputC'] as XYPoint);
-        anglesTest(_actual, elem['expectedOutput'] as Angles?);
+        var _actual = triangleSidesXY(elem['inputA'] as XYPoint, elem['inputB'] as XYPoint, elem['inputC'] as XYPoint);
+        sidesTest(_actual, elem['expectedOutput'] as Sides);
       });
     }
   });
