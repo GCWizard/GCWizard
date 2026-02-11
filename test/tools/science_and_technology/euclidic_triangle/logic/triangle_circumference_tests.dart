1efa04d9b98341acf00130e45e437fd6f3b61179
tmz <thomas@familiezimmermann.de>
2026-02-12 00:00:37 +0100

    add tests

---
+++ test/tools/science_and_technology/euclidic_triangle/logic/triangle_circumference_tests.dart
@@ -0,0 +1,28 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:gc_wizard/tools/science_and_technology/euclidic_triangle/logic/triangle.dart';
+
+import 'triangle_tests.dart';
+
+void main() {
+  group("triangle.triangleCircumferenceXY:", () {
+    List<Map<String, Object?>> _inputsToExpected = [
+      {'inputA': XYPoint(x: 0, y: 0), 'inputB': XYPoint(x: 0, y: 0), 'inputC': XYPoint(x: 0, y: 0),
+        'expectedOutput': 0},
+      {'inputA': XYPoint(x: 1, y: 1), 'inputB': XYPoint(x: 1, y: 1), 'inputC': XYPoint(x: 1, y: 1),
+        'expectedOutput': 0},
+      {'inputA': XYPoint(x: 1, y: 1), 'inputB': XYPoint(x: 2, y: 2), 'inputC': XYPoint(x: 3, y: 3),
+        'expectedOutput': 4 * 1.4142135623730951},
+
+      {'inputA': XYPoint(x: 0, y: 0), 'inputB': XYPoint(x: 0, y: 3), 'inputC': XYPoint(x: 4, y: 0),
+        'expectedOutput': 12},
+    ];
+
+    for (var elem in _inputsToExpected) {
+      test('input: ${toString(elem['inputA'])} ${toString(elem['inputB'])} ${toString(elem['inputC'])}', () {
+        var _actual = triangleCircumferenceXY(elem['inputA'] as XYPoint, elem['inputB'] as XYPoint, elem['inputC'] as XYPoint);
+        expect(_actual, elem['expectedOutput']);
+      });
+    }
+  });
+}
+
