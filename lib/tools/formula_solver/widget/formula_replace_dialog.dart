a427104c3b6ca0b4b7678e489d9eb89f64bda7c6
Mike-3 <Mike.Binnemann@web.de>
2026-03-10 19:00:56 +0100

    modify variable coord formula

--- lib/tools/formula_solver/widget/formula_replace_dialog.dart
+++ lib/tools/formula_solver/widget/formula_replace_dialog.dart
@@ -2,7 +2,7 @@ part of 'package:gc_wizard/tools/formula_solver/widget/formula_solver_formulagro
 
 List<String> _newFormulas = [];
 
-void _showFormulaReplaceDialog(BuildContext context, List<Formula> formulas,
+void showFormulaReplaceDialog(BuildContext context, List<Formula> formulas,
     {required void Function(List<Formula>) onOkPressed}) {
   var _output = formulas.map((formula) => Formula.fromFormula(formula)).toList();
 
