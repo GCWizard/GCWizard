71f0f070042c1598575ecd8aebda6b97d1f4f6c7
Mike-3 <Mike.Binnemann@web.de>
2026-03-19 18:26:28 +0100

    check empty input

--- lib/tools/science_and_technology/number_sequences/look_and_say/logic/look_and_say.dart
+++ lib/tools/science_and_technology/number_sequences/look_and_say/logic/look_and_say.dart
@@ -1,7 +1,9 @@
 
-String lookAndSay(String str) {
+String lookAndSay(String input) {
+  if (input.isEmpty) return '';
+
   final regex = RegExp(r'(.)\1*');
-  return str.replaceAllMapped(regex, (match) {
+  return input.replaceAllMapped(regex, (match) {
     final seq = match.group(0)!;
     final p1 = match.group(1)!;
     return '${seq.length}$p1';
