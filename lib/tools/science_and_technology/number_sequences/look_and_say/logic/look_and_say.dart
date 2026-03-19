6058dd2d9ea1af0ab2c46c857ed38edbd2d76a3b
Mike-3 <Mike.Binnemann@web.de>
2026-03-19 20:03:18 +0100

    Update look_and_say.dart

--- lib/tools/science_and_technology/number_sequences/look_and_say/logic/look_and_say.dart
+++ lib/tools/science_and_technology/number_sequences/look_and_say/logic/look_and_say.dart
@@ -15,31 +15,30 @@ String lookAndSay(String input) {
 }
 
 String lookAndSayReverse(String input) {
-  String current = input;
+  var current = input;
+  if (input.length % 2 != 0) return 'invalid input';
 
   while (true) {
     if (current.length % 2 != 0) break;
 
-    String next = _lookAndSayReverseHelper(current);
-    if (next == '-1') return 'invalid input';
-    
-    if (lookAndSay(next) != current) {
-      return current;
-    }
+    var next = _lookAndSayReverseHelper(current);
+    if (next == null) return 'invalid input';
+
+    if (lookAndSay(next) != current) return current;
     if (next == current) break;
     current = next;
   }
   return current;
 }
 
-String _lookAndSayReverseHelper(String input) {
-  if (input.length % 2 != 0) return '-1';
+String? _lookAndSayReverseHelper(String input) {
+  if (input.length % 2 != 0) return input;
 
   var result = '';
   for (int i = 0; i < input.length; i += 2) {
-    if (!input[i].isNumber) return '-1';
-    int count = int.parse(input[i]);
-    String digit = input[i + 1];
+    if (!input[i].isNumber) return null;
+    var count = int.parse(input[i]);
+    var digit = input[i + 1];
     result += digit * count;
   }
   return result;
@@ -53,5 +52,11 @@ void main() {
   // 1412211512 2222122222
   // 22 22
   // 1113122113121113222112311311222113 11123
+ print(lookAndSayReverse("1211"));
+ print(lookAndSayReverse("111321"));
  print(lookAndSayReverse("1412211512"));
+ print(lookAndSayReverse("22"));
+ print(lookAndSayReverse("1113122113121113222112311311222113"));
+ print(lookAndSayReverse("3a"));
+ print(lookAndSayReverse("aa"));
 }
