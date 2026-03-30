e790fcd1f4854fb555ed8ac9041462c66c32a94e
Mike-3 <Mike.Binnemann@web.de>
2026-03-30 17:43:02 +0200

    format code

--- lib/tools/science_and_technology/number_sequences/_common/logic/number_sequence_containsdigits.dart
+++ lib/tools/science_and_technology/number_sequences/_common/logic/number_sequence_containsdigits.dart
@@ -1,8 +1,8 @@
 part of 'package:gc_wizard/tools/science_and_technology/number_sequences/_common/logic/number_sequence.dart';
 
 PositionOfSequenceOutput numberSequencesGetFirstPositionOfSequence(
-    NumberSequencesMode sequence, String? check, int maxIndex,
-    {bool checkMode = false}) {
+    NumberSequencesMode sequence, String? check, int maxIndex, {bool checkMode = false}) {
+
   if (check == null || check.isEmpty) {
     return PositionOfSequenceOutput('-1', 0, 0);
   }
