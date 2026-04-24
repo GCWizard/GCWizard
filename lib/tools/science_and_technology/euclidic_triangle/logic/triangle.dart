c6d68ec2a131e73a388fcb633d801d7443c908fe
S-Man42 <mark@four-two.de>
2026-04-24 12:54:24 +0200

    fix triangles

--- lib/tools/science_and_technology/euclidic_triangle/logic/triangle.dart
+++ lib/tools/science_and_technology/euclidic_triangle/logic/triangle.dart
@@ -72,7 +72,7 @@ const Map<int, List<String>> TRIANGLES_SW_TEXT = {
   6: ['triangle_euclidic_s', 'triangle_euclidic_w', 'triangle_euclidic_w'],
 };
 
-const Map<String, String> TRIANGLE_LABLES = {
+Map<String, String> TRIANGLE_LABLES = {
   'LEGEND': 'triangle_output_legend',
   'COORDINATES': 'gcwizard_script_help_coordinates',
   'SIDES': 'triangle_output_sides',
