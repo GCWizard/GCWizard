e7e496b45d2928d79395c13212125f895770d40f
t-m-z <thomas@familiezimmermann.de>
2026-04-28 20:34:18 +0200

    remove translations

--- lib/tools/science_and_technology/bingo_calls/widget/bingo_calls.dart
+++ lib/tools/science_and_technology/bingo_calls/widget/bingo_calls.dart
@@ -1,8 +1,4 @@
-import 'dart:collection';
-
 import 'package:flutter/material.dart';
-import 'package:gc_wizard/application/i18n/logic/app_localizations.dart';
-import 'package:gc_wizard/common_widgets/dropdowns/gcw_dropdown.dart';
 import 'package:gc_wizard/common_widgets/outputs/gcw_columned_multiline_output.dart';
 import 'package:gc_wizard/common_widgets/outputs/gcw_default_output.dart';
 import 'package:gc_wizard/tools/science_and_technology/bingo_calls/logic/bingo_calls.dart';
@@ -16,8 +12,6 @@ class BingoCalls extends StatefulWidget {
 
 class _BingoCallsState extends State<BingoCalls> {
 
-  BINGOCALLS_LANGUAGES _currentLanguage = BINGOCALLS_LANGUAGES.EN;
-  
   @override
   void initState() {
     super.initState();
@@ -31,40 +25,13 @@ class _BingoCallsState extends State<BingoCalls> {
 
   @override
   Widget build(BuildContext context) {
-    // TMZ (6/2025): for the time beeing there exists only an english and german version
-    // if there exists Bingo calls in other languages the default language should be set
-
-    return Column(
-      children: <Widget>[
-        GCWDropDown<BINGOCALLS_LANGUAGES>(
-          value: _currentLanguage,
-          onChanged: (value) {
-            setState(() {
-              _currentLanguage = value;
-            });
-          },
-          items: SplayTreeMap<BINGOCALLS_LANGUAGES, String>.from(
-              BINGOCALLS_LANGUAGE_LIST,
-                  (keys1, keys2) =>
-                  i18n(context, BINGOCALLS_LANGUAGE_LIST[keys1]!).compareTo(i18n(context, BINGOCALLS_LANGUAGE_LIST[keys2]!)))
-              .entries
-              .map((mode) {
-            return GCWDropDownMenuItem(
-              value: mode.key,
-              child: i18n(context, mode.value),
-            );
-          }).toList(),
-        ),
-        _buildOutput(context)
-      ],
-    );
+    return _buildOutput(context);
   }
 
   Widget _buildOutput(BuildContext context) {
-    Map<String, BingoCall> bingoCalls = BINGO_CALLS[_currentLanguage]!;
     List<List<String>> output = [];
 
-    bingoCalls.forEach((k, v) {
+    BINGO_CALLS_EN.forEach((k, v) {
       output.add([k, v.title.join('\n'), v.description]);
     });
 
